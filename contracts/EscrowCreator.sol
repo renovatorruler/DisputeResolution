e23dedb1929dd2d769e69882fc931468d680c748
Prashant Singh Pawar <prashantpawar@gmail.com>
2016-04-23 00:42:16 -0400

    Changing sha256 to sha3 for key generation

--- contracts/EscrowCreator.sol
+++ contracts/EscrowCreator.sol
@@ -17,7 +17,7 @@ contract EscrowCreator {
     }
 
     function initiateCreation(address buyerAddress, address sellerAddress, uint amount) returns (bytes32 token) {
-        token = sha256(buyerAddress, sellerAddress, amount);
+        token = sha3(buyerAddress, sellerAddress, amount);
         Entity memory buyer = Entity(buyerAddress, false);
         Entity memory seller = Entity(sellerAddress, true);
         contracts[token] = EscrowInfo(buyer, seller, amount);
