eee790bb2909857cc48b9c07805b7429e574db45
Prashant Singh Pawar <prashantpawar@gmail.com>
2016-12-17 21:16:11 -0500

    formatted webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
 module.exports = {
-    entry: './src/app.ts',
-    output: {
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
-    },
-    devtool: 'source-map',
-    devServer: {
-        inline: true,
-        hot: false
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.ts$/,
-                loader: 'awesome-typescript-loader'
-            }
-        ]
-    }
+  entry: './src/app.ts',
+  output: {
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
+  },
+  devtool: 'source-map',
+  devServer: {
+    inline: true,
+    hot: false
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        loader: 'awesome-typescript-loader'
+      }
+    ]
+  }
 }
