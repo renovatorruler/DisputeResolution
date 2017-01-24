009071c8ebd2f5f6cc4a8d87668a2d7d5f7efdaf
Prashant Singh Pawar <prashantpawar@gmail.com>
2017-01-23 22:45:23 -0500

    Added helper contracts

--- contracts/BrehonContract.sol
+++ contracts/BrehonContract.sol
@@ -1,10 +1,15 @@
 pragma solidity ^0.4.2;
 
-contract BrehonContract {
+import "./owned.sol";
+import "./priced.sol";
+import "./stateMachine.sol";
+import "./accessRestricted.sol";
+
+/// @title Arbitrated Contract
+contract BrehonContract is priced, owned, stateMachine {
   address partyA;
   address partyB;
   address primaryBrehon;
   address secondaryBrehon;
   address tertiaryBrehon;
-
 }
---
+++ contracts/stateMachine.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.4.2;
+
+contract stateMachine {
+  enum Stages {
+    Negotiation,
+    Execution,
+    Disputed,
+    Resolved,
+    Appealed,
+    Completed
+  }
+
+  Stages public stage = Stages.Negotiation;
+
+  modifier atStage(Stages _stage) {
+    if (stage != _stage) throw;
+    _;
+  }
+
+  function nextStage() internal {
+    stage = Stages(uint(stage) + 1);
+  }
+}
+
