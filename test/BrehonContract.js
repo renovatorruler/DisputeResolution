15d3b1ca1e2c41f224fa4d62c5e79a5ac576e07e
Prashant Singh Pawar <prashantpawar@gmail.com>
2017-02-03 16:08:02 -0500

    Added more test coverage

--- test/BrehonContract.js
+++ test/BrehonContract.js
@@ -6,11 +6,46 @@ contract('BrehonContract', function (accounts) {
     });
   });
 
-  it('should set partys properly', function () {
+  it("should set partyA's address properly", function () {
     var brehonContract = BrehonContract.deployed();
     brehonContract.partyA.call().then(function (partyA) {
-      console.log(partyA);
-      //assert(appealLevel.valueOf(), -1);
+      assert(partyA[0], accounts[0]);
+    });
+  });
+
+  it("should set partyA's deposit to 0", function () {
+    var brehonContract = BrehonContract.deployed();
+    brehonContract.partyA.call().then(function (partyA) {
+      assert(partyA[1].valueOf(), 0);
+    });
+  });
+
+  it("should set partyA's contractAccepted to false", function () {
+    var brehonContract = BrehonContract.deployed();
+    brehonContract.partyA.call().then(function (partyA) {
+      console.log(partyA[2]);
+      assert(partyA[2], false);
+    });
+  });
+
+  it("should set partyA' primaryBrehonApproval to false", function () {
+    var brehonContract = BrehonContract.deployed();
+    brehonContract.partyA.call().then(function (partyA) {
+      assert(partyA[3], false);
+    });
+  });
+
+  it("should set partyA's secondaryBrehonApproval to false", function () {
+    var brehonContract = BrehonContract.deployed();
+    brehonContract.partyA.call().then(function (partyA) {
+      assert(partyA[4], false);
+    });
+  });
+
+  it("should set partyA's tertiaryBrehonApproval to false", function () {
+    var brehonContract = BrehonContract.deployed();
+    brehonContract.partyA.call().then(function (partyA) {
+      assert(partyA[5], false);
     });
   });
 });
