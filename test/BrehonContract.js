fbaae1ab9d08f368ba1856abc3dd9d5587f04627
Prashant Singh Pawar <prashantpawar@gmail.com>
2017-02-05 00:34:57 -0500

    - Added testing on deposit on unauthorized addresses

--- test/BrehonContract.js
+++ test/BrehonContract.js
@@ -486,3 +486,85 @@ contract('BrehonContract should accept funds from partyB', function (accounts) {
     });
   });
 });
+
+contract('BrehonContract shouldnt\'t accept funds from unauthorized addresses', function (accounts) {
+  it('like from the primaryBrehon', function () {
+    var brehonContract;
+    return BrehonContract.deployed().then(function (instance) {
+      brehonContract = instance;
+      return brehonContract.deposit({from: defaults.primaryBrehon_addr, value: defaults.transactionAmount});
+    }).then(function () {
+      return brehonContract.partyA.call().then(function (partyA) {
+        assert.equal(partyA[PartyStruct.deposit].valueOf(), 0,
+          "partyA's contractAccepted is incorrectly set");
+      });
+    }).then(function () {
+      return brehonContract.partyB.call().then(function (partyB) {
+        assert.equal(partyB[PartyStruct.deposit].valueOf(), 0,
+          "partyB's contractAccepted is incorrectly set");
+      });
+    }).catch(function (err) {
+      assert.isNotNull(err, "Exception was not thrown when a primaryBrehon tried to deposit funds to the contract");
+    });
+  });
+
+  it('or from the secondaryBrehon', function () {
+    var brehonContract;
+    return BrehonContract.deployed().then(function (instance) {
+      brehonContract = instance;
+      return brehonContract.deposit({from: defaults.secondaryBrehon_addr, value: defaults.transactionAmount});
+    }).then(function () {
+      return brehonContract.partyA.call().then(function (partyA) {
+        assert.equal(partyA[PartyStruct.deposit].valueOf(), 0,
+          "partyA's contractAccepted is incorrectly set");
+      });
+    }).then(function () {
+      return brehonContract.partyB.call().then(function (partyB) {
+        assert.equal(partyB[PartyStruct.deposit].valueOf(), 0,
+          "partyB's contractAccepted is incorrectly set");
+      });
+    }).catch(function (err) {
+      assert.isNotNull(err, "Exception was not thrown when a secondaryBrehon tried to deposit funds to the contract");
+    });
+  });
+
+  it('or from the tertiaryBrehon', function () {
+    var brehonContract;
+    return BrehonContract.deployed().then(function (instance) {
+      brehonContract = instance;
+      return brehonContract.deposit({from: defaults.tertiaryBrehon_addr, value: defaults.transactionAmount});
+    }).then(function () {
+      return brehonContract.partyA.call().then(function (partyA) {
+        assert.equal(partyA[PartyStruct.deposit].valueOf(), 0,
+          "partyA's contractAccepted is incorrectly set");
+      });
+    }).then(function () {
+      return brehonContract.partyB.call().then(function (partyB) {
+        assert.equal(partyB[PartyStruct.deposit].valueOf(), 0,
+          "partyB's contractAccepted is incorrectly set");
+      });
+    }).catch(function (err) {
+      assert.isNotNull(err, "Exception was not thrown when a tertiaryBrehon tried to deposit funds to the contract");
+    });
+  });
+
+  it('or from a rando', function () {
+    var brehonContract;
+    return BrehonContract.deployed().then(function (instance) {
+      brehonContract = instance;
+      return brehonContract.deposit({from: accounts[6], value: defaults.transactionAmount});
+    }).then(function () {
+      return brehonContract.partyA.call().then(function (partyA) {
+        assert.equal(partyA[PartyStruct.deposit].valueOf(), 0,
+          "partyA's contractAccepted is incorrectly set");
+      });
+    }).then(function () {
+      return brehonContract.partyB.call().then(function (partyB) {
+        assert.equal(partyB[PartyStruct.deposit].valueOf(), 0,
+          "partyB's contractAccepted is incorrectly set");
+      });
+    }).catch(function (err) {
+      assert.isNotNull(err, "Exception was not thrown when a rando tried to deposit funds to the contract");
+    });
+  });
+});
