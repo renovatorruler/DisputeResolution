ab5593e594d7cdd0e580b9f8c351359cd5ceb1df
Prashant Singh Pawar <prashantpawar@gmail.com>
2017-02-04 19:12:14 -0500

    Added deployment settings in config

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,23 +1,24 @@
 //var ConvertLib = artifacts.require("./ConvertLib.sol");
 //var MetaCoin = artifacts.require("./MetaCoin.sol");
 var BrehonContract = artifacts.require("./BrehonContract.sol");
+var defaults = require('../config/deployment_settings.js').defaults;
 
 module.exports = function(deployer) {
   //deployer.deploy(ConvertLib);
   //deployer.link(ConvertLib, MetaCoin);
   //deployer.deploy(MetaCoin);
   deployer.deploy(BrehonContract,
-    '0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1', //PartyA
-    '0xffcf8fdee72ac11b5c542428b35eef5769c409f0', //PartyB
-    '10000000000000000000', //transactionAmount
-    '0x22d491bde2303f2f43325b2108d26f1eaba1e32b', //PrimaryBrehon
-    '100000000000000000', //PrimaryBrehon Fixed Fee
-    '1000000000000000000', //PrimaryBrehon Dispute Fee
-    '0xe11ba2b4d45eaed5996cd0823791e0c93114882d', //SecondaryBrehon
-    '100000000000000000', //SecondaryBrehon Fixed Fee
-    '1000000000000000000', //SecondaryBrehon Dispute Fee
-    '0xd03ea8624c8c5987235048901fb614fdca89b117', //TertiaryBrehon
-    '100000000000000000', //TertiaryBrehon Fixed Fee
-    '1000000000000000000' //TertiaryBrehon Dispute Fee
+    defaults.partyA_addr,
+    defaults.partyB_addr,
+    defaults.transactionAmount,
+    defaults.primaryBrehon_addr,
+    defaults.primaryBrehon_fixedFee,
+    defaults.primaryBrehon_disputeFee,
+    defaults.secondaryBrehon_addr,
+    defaults.secondaryBrehon_fixedFee,
+    defaults.secondaryBrehon_disputeFee,
+    defaults.tertiaryBrehon_addr,
+    defaults.tertiaryBrehon_fixedFee,
+    defaults.tertiaryBrehon_disputeFee
   );
 };
