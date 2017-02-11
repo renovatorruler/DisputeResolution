a3623b615cc920efa7c54ae21ea4e6111cead349
Prashant Singh Pawar <prashantpawar@gmail.com>
2017-02-11 05:15:17 +0000

    Added abstraction to raiseDispute

--- test/BrehonContract_raiseDispute.js
+++ test/BrehonContract_raiseDispute.js
@@ -1,3 +1,5 @@
+const R = require('ramda');
+
 const BrehonContract = artifacts.require("./BrehonContract.sol");
 const defaults = require('../config/deployment_settings.js').defaults;
 
@@ -7,6 +9,19 @@ const getMinimumContractAmt = contractHelpers.getMinimumContractAmt;
 const PartyStruct = contractHelpers.PartyStruct;
 const BrehonStruct = contractHelpers.BrehonStruct;
 
+const raiseDispute = R.curry(function raiseDispute(disputing_party_addr, brehonContract) {
+  return brehonContract.raiseDispute({
+    from: disputing_party_addr
+  });
+});
+
+const startContractAndRaiseDispute = R.curry(function startContractAndRaiseDispute(party_contributions, starting_party_addr, disputing_party_addr, brehonContract) {
+    return startContract(party_contributions)(starting_party_addr)(brehonContract)
+      .then(() => {
+          return raiseDispute(disputing_party_addr)(brehonContract);
+      });
+});
+
 contract('BrehonContract should allow partyA to raise the dispute', (accounts) => {
   it('when partyA deposited the funds', () => {
     var brehonContract;
@@ -15,20 +30,18 @@ contract('BrehonContract should allow partyA to raise the dispute', (accounts) =
         brehonContract = instance;
         return instance;
       })
-      .then(startContract([{
-        addr: defaults.partyA_addr,
-        value:getMinimumContractAmt(defaults)}])(defaults.partyA_addr))
-      .then(() => {
-        return brehonContract.raiseDispute({
-            from: defaults.partyA_addr
-        })
-      })
+      .then(startContractAndRaiseDispute(
+        [{
+          addr: defaults.partyA_addr,
+          value: getMinimumContractAmt(defaults)
+        }], defaults.partyA_addr, defaults.partyA_addr))
       .then(() => {
         return brehonContract.stage.call().then((stage) => {
             assert.equal(stage.valueOf(), 2, "stage is not set to Stages.Dispute");
         });
       })
       .catch((err) => {
+        console.log(err);
         assert.isNull(err, "Exception was thrown when partyA tried to raise a dispute");
       });
   });
@@ -126,8 +139,26 @@ contract('BrehonContract raiseDispute should only be raised at Execution stage',
       .then(() => {
         return brehonContract.raiseDispute({from: defaults.partyA_addr});
       })
-    .catch((err) => {
-      assert.isNotNull(err, "Exception was not thrown when raiseDispute was triggerred at the Negotiation stage");
-    });
+      .catch((err) => {
+        assert.isNotNull(err, "Exception was not thrown when raiseDispute was triggerred at the Negotiation stage");
+      });
   });
+
+  it('by preventing it from being raised at Dispute stage', () => {
+    var brehonContract;
+    return BrehonContract.deployed()
+      .then(function captureReference(instance) {
+        brehonContract = instance;
+        return instance;
+      })
+      .then(() => {
+        return brehonContract.raiseDispute({from: defaults.partyA_addr});
+      })
+      .catch((err) => {
+        assert.isNotNull(err, "Exception was not thrown when raiseDispute was triggerred at the Negotiation stage");
+      });
+  });
+});
+
+contract('BrehonContract raiseDispute should not be raised by unauthorized addresses', (accounts) => {
 });
