cb6dc7c4201afe0de71709079c35c563f9622788
Prashant Singh Pawar <prashantpawar@gmail.com>
2017-02-11 05:14:56 +0000

    Reduce the deployment requirement for the contracts

--- config/deployment_settings.js
+++ config/deployment_settings.js
@@ -1,14 +1,14 @@
 exports.defaults = {
     'partyA_addr': '0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1', //PartyA
     'partyB_addr': '0xffcf8fdee72ac11b5c542428b35eef5769c409f0', //PartyB
-    'transactionAmount': '1000000000000000', //transactionAmount
+    'transactionAmount': '5000', //transactionAmount
     'primaryBrehon_addr': '0x22d491bde2303f2f43325b2108d26f1eaba1e32b', //PrimaryBrehon
-    'primaryBrehon_fixedFee': '1000000000000', //PrimaryBrehon Fixed Fee
-    'primaryBrehon_disputeFee': '10000000000000', //PrimaryBrehon Dispute Fee
+    'primaryBrehon_fixedFee': '10', //PrimaryBrehon Fixed Fee
+    'primaryBrehon_disputeFee': '100', //PrimaryBrehon Dispute Fee
     'secondaryBrehon_addr': '0xe11ba2b4d45eaed5996cd0823791e0c93114882d', //SecondaryBrehon
-    'secondaryBrehon_fixedFee': '1000000000000', //SecondaryBrehon Fixed Fee
-    'secondaryBrehon_disputeFee': '10000000000000', //SecondaryBrehon Dispute Fee
+    'secondaryBrehon_fixedFee': '10', //SecondaryBrehon Fixed Fee
+    'secondaryBrehon_disputeFee': '100', //SecondaryBrehon Dispute Fee
     'tertiaryBrehon_addr': '0xd03ea8624c8c5987235048901fb614fdca89b117', //TertiaryBrehon
-    'tertiaryBrehon_fixedFee': '1000000000000', //TertiaryBrehon Fixed Fee
-    'tertiaryBrehon_disputeFee': '10000000000000' //TertiaryBrehon Dispute Fee
+    'tertiaryBrehon_fixedFee': '10', //TertiaryBrehon Fixed Fee
+    'tertiaryBrehon_disputeFee': '100' //TertiaryBrehon Dispute Fee
 }
