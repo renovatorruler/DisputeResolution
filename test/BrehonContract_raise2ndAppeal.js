457d599dc14c3e1ff7e4ac29568d7736f22e2945
Prashant Singh Pawar <prashantpawar@gmail.com>
2017-02-15 22:57:48 -0500

    - Added tests for partyB being raise the second appeal

--- test/BrehonContract_raise2ndAppeal.js
+++ test/BrehonContract_raise2ndAppeal.js
@@ -86,6 +86,72 @@ contract('BrehonContract should allow partyA to raise an appeal', (accounts) => 
 
 contract('BrehonContract should allow partyB to raise an appeal', (accounts) => {
   it('when he is dissatisfied by the resolution provided by the secondaryBrehon', () => {
+    var brehonContract;
+    return BrehonContract.deployed()
+      .then(function captureReference(instance) {
+        brehonContract = instance;
+        return instance;
+      })
+      .then(startContractAndRaiseDispute(
+        [{
+          addr: defaults.partyA_addr,
+          value: getMinimumContractAmt(defaults)
+        }], defaults.partyA_addr, defaults.partyA_addr))
+      .then(function adjudicate() {
+        return brehonContract.adjudicate(
+            getSplitForPrimaryBrehon(100),
+            getSplitForPrimaryBrehon(0),
+            {from: defaults.primaryBrehon_addr}
+        );
+      })
+      .then(function raiseAppeal() {
+        return brehonContract.raiseAppeal(
+            {from: defaults.partyA_addr}
+        );
+      })
+      .then(function adjudicate() {
+        return brehonContract.adjudicate(
+            getSplitForSecondaryBrehon(100),
+            getSplitForSecondaryBrehon(0),
+            {from: defaults.secondaryBrehon_addr}
+        );
+      })
+      .then(function raise2ndAppeal() {
+        return brehonContract.raise2ndAppeal(
+            {from: defaults.partyB_addr}
+        );
+      })
+      .then(function verifyAppealRaisedEvent(result) {
+        var appealRaisedEvent = R.find(R.propEq('event', 'AppealRaised'), result.logs);
+        assert.equal(appealRaisedEvent.args._appealLevel, 2,
+          "AppealRaised event did not correctly provide the appealLevel");
+        assert.equal(appealRaisedEvent.args._activeBrehon, defaults.tertiaryBrehon_addr,
+          "AppealRaised event did not correctly provide the activeBrehon's address");
+        assert.isDefined(appealRaisedEvent, "ContractStarted event was not emitted");
+        return result;
+      })
+      .then(function verifyStage() {
+        return brehonContract.stage.call().then((stage) => {
+            assert.equal(stage.valueOf(), 2, "stage is not set to Stages.Dispute");
+        });
+      })
+      .then(function verifyAppealLevel() {
+        return brehonContract.appealLevel.call().then((appealLevel) => {
+            assert.equal(appealLevel.valueOf(), 2, "Appeal level not set correctly");
+        });
+      })
+      .then(function verifyActiveBrehon() {
+        return brehonContract.activeBrehon.call().then((activeBrehon) => {
+            assert.equal(activeBrehon[BrehonStruct.contractAccepted], true, 'activeBrehon\'s contractAccepted should be set to true');
+            assert.equal(activeBrehon[BrehonStruct.addr], defaults.tertiaryBrehon_addr, 'activeBrehon\'s address not set to tertiaryBrehon correctly');
+            assert.equal(activeBrehon[BrehonStruct.fixedFee], defaults.tertiaryBrehon_fixedFee, 'activeBrehon\'s fixedFee not set to tertiaryBrehon\'s fixedFee');
+            assert.equal(activeBrehon[BrehonStruct.disputeFee], defaults.tertiaryBrehon_disputeFee, 'activeBrehon\'s disputeFee not set to tertiaryBrehon\'s disputeFee');
+        });
+      })
+      .catch(function handleException(err) {
+        console.log(err);
+        assert.isNull(err, "Exception was thrown when partyA tried to raise an appeal");
+      });
   });
 });
 
