b74eb239ccf887b73ddd42892dd4009571d7464b
Prashant Singh Pawar <prashantpawar@gmail.com>
2017-02-19 15:17:01 -0500

    - Added test for partyB to be able to propose a settlement

--- test/BrehonContract_proposeSettlement.spec.js
+++ test/BrehonContract_proposeSettlement.spec.js
@@ -83,3 +83,55 @@ contract('BrehonContract should allow partyA to propose a settlement', (accounts
       });
   });
 });
+
+contract('BrehonContract should allow partyA to propose a settlement', (accounts) => {
+  const settlement = {
+    'partyA': getSplitForPrimaryBrehon(40),
+    'partyB': getSplitForPrimaryBrehon(60)
+  };
+  it('at Dispute stage', () => {
+    var brehonContract;
+    return BrehonContract.deployed()
+      .then(function captureReference(instance) {
+        brehonContract = instance;
+        return instance;
+      })
+      .then(startContractAndRaiseDispute(
+        [{
+          addr: defaults.partyA_addr,
+          value: getMinimumContractAmt(defaults)
+        }], defaults.partyA_addr, defaults.partyA_addr))
+      .then(function proposeSettlement() {
+        return brehonContract.proposeSettlement(
+          settlement.partyA,
+          settlement.partyB,
+          { from: defaults.partyB_addr }
+        );
+      })
+      .then(verifyEvent('SettlementProposed', {
+        '_proposingParty': defaults.partyB_addr,
+        '_awardPartyA': settlement.partyA,
+        '_awardPartyB': settlement.partyB,
+      }))
+      .then(function verifySettlementRecord() {
+        return brehonContract.proposedSettlement.call().then((proposedSettlement) => {
+          assert.equal(proposedSettlement[ResolutionStruct.awardPartyA].valueOf(),
+            settlement.partyA.valueOf(), 'proposedSettlement not recorded correctly for partyA');
+          assert.equal(proposedSettlement[ResolutionStruct.awardPartyB].valueOf(),
+            settlement.partyB.valueOf(), 'proposedSettlement not recorded correctly for partyB');
+          assert.equal(proposedSettlement[ResolutionStruct.proposerAddr],
+            defaults.partyB_addr, 'proposedSettlement\'s proposer address not recorded correctly');
+          assert.equal(proposedSettlement[ResolutionStruct.partyAAccepted],
+            false, 'proposedSettlement should set calling party\'s acceptance to true');
+          assert.equal(proposedSettlement[ResolutionStruct.partyBAccepted],
+            true, 'proposedSettlement should set non-calling party\'s acceptance to false');
+        });
+      })
+      .catch(function handleException(err) {
+        console.log(err);
+        assert.isNull(err, 'Exception was thrown when partyA tried to raise an appeal');
+      });
+  });
+});
+
+// BrehonContract should only be triggered at dispute stages
