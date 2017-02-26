fee2b60384c56a3f76f49fbce246f0827bc41e0e
Prashant Singh Pawar <prashantpawar@gmail.com>
2017-02-26 01:12:43 -0500

    - Attempt to integrate webpack into the flow

---
+++ app/javascripts/BrehonAPI.js
@@ -0,0 +1,19 @@
+import contract from 'truffle-contract';
+
+// Import our contract artifacts and turn them into usable abstractions.
+import BrehonContractArtifact from '../../build/contracts/BrehonContract.json';
+
+export default class BrehonAPI {
+  constructor(web3provider) {
+    // BrehonContract is our usable abstraction, which we'll use through the code below.
+    this.brehonContract = contract(BrehonContractArtifact);
+
+    // Bootstrap the BrehonContract abstraction for Use.
+    this.brehonContract.setProvider(web3provider);
+  }
+
+  start() {
+    console.log('started');
+    return this;
+  }
+}
--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -1,55 +1,97 @@
-var accounts;
-var account;
+// Import libraries we need.
+import Web3 from 'web3';
 
-function setStatus(message) {
-  var status = document.getElementById("status");
-  status.innerHTML = message;
-}
+import BrehonAPI from './BrehonAPI';
 
-function refreshBalance() {
-  var meta = MetaCoin.deployed();
+// Import the page's CSS. Webpack will know what to do with it.
+import '../stylesheets/app.css';
 
-  meta.getBalance.call(account, {from: account}).then(function(value) {
-    var balance_element = document.getElementById("balance");
-    balance_element.innerHTML = value.valueOf();
-  }).catch(function(e) {
-    console.log(e);
-    setStatus("Error getting balance; see log.");
-  });
-}
+// The following code is simple to show off interacting with your contracts.
+// As your needs grow you will likely need to change its form and structure.
+// For application bootstrapping, check out window.addEventListener below.
+let accounts;
+let account;
 
-function sendCoin() {
-  var meta = MetaCoin.deployed();
+/**
+window.App = {
+  start: () => {
+    const self = this;
 
-  var amount = parseInt(document.getElementById("amount").value, 10);
-  var receiver = document.getElementById("receiver").value;
+    // Get the initial account balance so it can be displayed.
+    web3.eth.getAccounts((err, accs) => {
+      if (err != null) {
+        console.error('There was an error fetching your accounts.');
+        return;
+      }
 
-  setStatus("Initiating transaction... (please wait)");
+      if (accs.length === 0) {
+        console.error('Couldn\'t get any accounts! Make sure your Ethereum client is configured correctly.');
+        return;
+      }
 
-  meta.sendCoin(receiver, amount, {from: account}).then(function() {
-    setStatus("Transaction complete!");
-    refreshBalance();
-  }).catch(function(e) {
-    console.log(e);
-    setStatus("Error sending coin; see log.");
-  });
-}
+      accounts = accs;
+      account = accounts[0];
 
-window.onload = function() {
-  web3.eth.getAccounts(function(err, accs) {
-    if (err !== null) {
-      alert("There was an error fetching your accounts.");
-      return;
-    }
+      self.refreshBalance();
+    });
+  },
 
-    if (accs.length === 0) {
-      alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
-      return;
-    }
+  setStatus: (message) => {
+    const status = document.getElementById('status');
+    status.innerHTML = message;
+  },
 
-    accounts = accs;
-    account = accounts[0];
+  refreshBalance: () => {
+    const self = this;
 
-    refreshBalance();
-  });
+    let meta;
+    BrehonContract.deployed().then((instance) => {
+      meta = instance;
+      return meta.getBalance.call(account, { from: account });
+    }).then((value) => {
+      const balanceEl = document.getElementById('balance');
+      balanceEl.innerHTML = value.valueOf();
+    }).catch((e) => {
+      console.error(e);
+      self.setStatus('Error getting balance; see log.');
+    });
+  },
+
+  sendCoin: () => {
+    const self = this;
+
+    const amount = parseInt(document.getElementById('amount').value, 10);
+    const receiver = document.getElementById('receiver').value;
+
+    this.setStatus('Initiating transaction... (please wait)');
+
+    let meta;
+    BrehonContract.deployed().then((instance) => {
+      meta = instance;
+      return meta.sendCoin(receiver, amount, { from: account });
+    }).then(() => {
+      self.setStatus('Transaction complete!');
+      self.refreshBalance();
+    }).catch((e) => {
+      console.error(e);
+      self.setStatus('Error sending coin; see log.');
+    });
+  },
 };
+**/
+
+window.addEventListener('load', () => {
+  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+  if (typeof web3 !== 'undefined') {
+    console.warn('Using web3 detected from external source. If you find that your accounts don\'t appear or you have 0 BrehonContract, ensure you\'ve configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask');
+    // Use Mist/MetaMask's provider
+    window.web3 = new Web3(web3.currentProvider);
+  } else {
+    console.warn('No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it\'s inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask');
+    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+    window.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+  }
+
+  const brehonApp = new BrehonAPI(web3.currentProvider);
+  brehonApp.start();
+});
