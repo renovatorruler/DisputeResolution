d14bd4b5d6243ea631cf23978a2aee8e4e831252
Prashant Singh Pawar <prashantpawar@gmail.com>
2017-03-02 22:21:31 -0500

    - Modularized Elm App

--- app/elm/Main.elm
+++ app/elm/Main.elm
@@ -1,39 +1,18 @@
 module Main exposing (..)
 
 import Html exposing (Html, div, text, program)
+import Msgs exposing (Msg)
+import Models exposing (Model)
+import Update exposing (update)
+import View exposing (view)
 
 
 -- MODEL
-
-type alias Model =
-  String
 
 init : ( Model, Cmd Msg)
 init =
   ( "Hello", Cmd.none )
 
-
--- MESSAGES
-
-type Msg
-  = NoOp
-
-
--- VIEW
-
-view : Model -> Html Msg
-view model =
-  div []
-      [ text model ]
-
-
--- UPDATE
-
-update : Msg -> Model -> ( Model, Cmd Msg)
-update msg model =
-  case msg of
-    NoOp ->
-      ( model, Cmd.none )
 
 -- SUBSCRIPTIONS
 
