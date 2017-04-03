a7a878ae544e351486add560ee946ac2ec2a273e
Prashant Singh Pawar <prashantpawar@gmail.com>
2017-04-02 21:37:57 -0400

    - Eslint fixe

--- test/BrehonContract_raiseDispute.spec.js
+++ test/BrehonContract_raiseDispute.spec.js
@@ -1,18 +1,16 @@
-const R = require('ramda');
-
-const BrehonContract = artifacts.require("./BrehonContract.sol");
+const BrehonContract = artifacts.require('./BrehonContract.sol');
 const defaults = require('../config/deployment_settings.js').defaults;
 
 const contractHelpers = require('../lib/contractHelpers.js');
-const startContract = contractHelpers.startContract;
+
 const startContractAndRaiseDispute = contractHelpers.startContractAndRaiseDispute;
 const getMinimumContractAmt = contractHelpers.getMinimumContractAmt;
-const PartyStruct = contractHelpers.PartyStruct;
+const verifyEvent = contractHelpers.verifyEvent;
 const BrehonStruct = contractHelpers.BrehonStruct;
 
-contract('BrehonContract should allow partyA to raise the dispute', (accounts) => {
+contract('BrehonContract should allow partyA to raise the dispute', () => {
   it('when partyA deposited the funds', () => {
-    var brehonContract;
+    let brehonContract;
     return BrehonContract.deployed()
       .then(function captureReference(instance) {
         brehonContract = instance;
@@ -21,22 +19,19 @@ contract('BrehonContract should allow partyA to raise the dispute', (accounts) =
       .then(startContractAndRaiseDispute(
         [{
           addr: defaults.partyA_addr,
-          value: getMinimumContractAmt(defaults)
+          value: getMinimumContractAmt(defaults),
         }], defaults.partyA_addr, defaults.partyA_addr))
-      .then(() => {
-        return brehonContract.stage.call().then((stage) => {
-            assert.equal(stage.valueOf(), 2, "stage is not set to Stages.Dispute");
-        });
-      })
+      .then(() => brehonContract.stage.call().then(stage =>
+        assert.equal(stage.valueOf(), 2, 'stage is not set to Stages.Dispute')))
       .catch((err) => {
-        assert.isNull(err, "Exception was thrown when partyA tried to raise a dispute");
+        assert.isNull(err, 'Exception was thrown when partyA tried to raise a dispute');
       });
   });
 });
 
-contract('BrehonContract should allow partyB to raise the dispute', (accounts) => {
+contract('BrehonContract should allow partyB to raise the dispute', () => {
   it('when partyB deposited the funds', () => {
-    var brehonContract;
+    let brehonContract;
     return BrehonContract.deployed()
       .then(function captureReference(instance) {
         brehonContract = instance;
@@ -45,22 +40,19 @@ contract('BrehonContract should allow partyB to raise the dispute', (accounts) =
       .then(startContractAndRaiseDispute(
         [{
           addr: defaults.partyB_addr,
-          value: getMinimumContractAmt(defaults)
+          value: getMinimumContractAmt(defaults),
         }], defaults.partyB_addr, defaults.partyB_addr))
-      .then(() => {
-        return brehonContract.stage.call().then((stage) => {
-            assert.equal(stage.valueOf(), 2, "stage is not set to Stages.Dispute");
-        });
-      })
+      .then(() => brehonContract.stage.call().then(stage =>
+        assert.equal(stage.valueOf(), 2, 'stage is not set to Stages.Dispute')))
       .catch((err) => {
-        assert.isNull(err, "Exception was thrown when partyB tried to raise a dispute");
+        assert.isNull(err, 'Exception was thrown when partyB tried to raise a dispute');
       });
   });
 });
 
-contract('BrehonContract should allow partyA to raise the dispute', (accounts) => {
+contract('BrehonContract should allow partyA to raise the dispute', () => {
   it('when partyB deposited the funds', () => {
-    var brehonContract;
+    let brehonContract;
     return BrehonContract.deployed()
       .then(function captureReference(instance) {
         brehonContract = instance;
@@ -69,22 +61,19 @@ contract('BrehonContract should allow partyA to raise the dispute', (accounts) =
       .then(startContractAndRaiseDispute(
         [{
           addr: defaults.partyB_addr,
-          value: getMinimumContractAmt(defaults)
+          value: getMinimumContractAmt(defaults),
         }], defaults.partyA_addr, defaults.partyA_addr))
-      .then(() => {
-        return brehonContract.stage.call().then((stage) => {
-            assert.equal(stage.valueOf(), 2, "stage is not set to Stages.Dispute");
-        });
-      })
+      .then(() => brehonContract.stage.call().then(stage =>
+        assert.equal(stage.valueOf(), 2, 'stage is not set to Stages.Dispute')))
       .catch((err) => {
-        assert.isNull(err, "Exception was thrown when partyA tried to raise a dispute");
+        assert.isNull(err, 'Exception was thrown when partyA tried to raise a dispute');
       });
   });
 });
 
-contract('BrehonContract should allow partyB to raise the dispute', (accounts) => {
+contract('BrehonContract should allow partyB to raise the dispute', () => {
   it('when partyA deposited the funds', () => {
-    var brehonContract;
+    let brehonContract;
     return BrehonContract.deployed()
       .then(function captureReference(instance) {
         brehonContract = instance;
@@ -93,37 +82,32 @@ contract('BrehonContract should allow partyB to raise the dispute', (accounts) =
       .then(startContractAndRaiseDispute(
         [{
           addr: defaults.partyA_addr,
-          value: getMinimumContractAmt(defaults)
+          value: getMinimumContractAmt(defaults),
         }], defaults.partyA_addr, defaults.partyB_addr))
-      .then(() => {
-        return brehonContract.stage.call().then((stage) => {
-            assert.equal(stage.valueOf(), 2, "stage is not set to Stages.Dispute");
-        });
-      })
+      .then(() => brehonContract.stage.call().then(stage =>
+        assert.equal(stage.valueOf(), 2, 'stage is not set to Stages.Dispute')))
       .catch((err) => {
-        assert.isNull(err, "Exception was thrown when partyB tried to raise a dispute");
+        assert.isNull(err, 'Exception was thrown when partyB tried to raise a dispute');
       });
   });
 });
 
-contract('BrehonContract raiseDispute should only be raised at Execution stage', (accounts) => {
+contract('BrehonContract raiseDispute should only be raised at Execution stage', () => {
   it('by preventing it from being raised at Negotiaton stage', () => {
-    var brehonContract;
+    let brehonContract;
     return BrehonContract.deployed()
       .then(function captureReference(instance) {
         brehonContract = instance;
         return instance;
       })
-      .then(() => {
-        return brehonContract.raiseDispute({from: defaults.partyA_addr});
-      })
+      .then(() => brehonContract.raiseDispute({ from: defaults.partyA_addr }))
       .catch((err) => {
-        assert.isNotNull(err, "Exception was not thrown when raiseDispute was triggerred at the Negotiation stage");
+        assert.isNotNull(err, 'Exception was not thrown when raiseDispute was triggerred at the Negotiation stage');
       });
   });
 
   it('by preventing it from being raised at Dispute stage', () => {
-    var brehonContract;
+    let brehonContract;
     return BrehonContract.deployed()
       .then(function captureReference(instance) {
         brehonContract = instance;
@@ -132,130 +116,95 @@ contract('BrehonContract raiseDispute should only be raised at Execution stage',
       .then(startContractAndRaiseDispute(
         [{
           addr: defaults.partyA_addr,
-          value: getMinimumContractAmt(defaults)
+          value: getMinimumContractAmt(defaults),
         }], defaults.partyA_addr, defaults.partyA_addr))
-      .then(() => {
-        return brehonContract.raiseDispute({from: defaults.partyA_addr});
-      })
+      .then(() => brehonContract.raiseDispute({ from: defaults.partyA_addr }))
       .catch((err) => {
-        assert.isNotNull(err, "Exception was not thrown when raiseDispute was triggerred at the Negotiation stage");
+        assert.isNotNull(err, 'Exception was not thrown when raiseDispute was triggerred at the Negotiation stage');
       });
   });
 });
 
 contract('BrehonContract raiseDispute should not be raised by unauthorized addresses', (accounts) => {
-  it('by preventing primaryBrehon from raising a dispute', () => {
-    var brehonContract;
-    return BrehonContract.deployed()
-      .then(function captureReference(instance) {
-        brehonContract = instance;
-        return instance;
-      })
-      .then(startContractAndRaiseDispute(
-        [{
-          addr: defaults.partyA_addr,
-          value: getMinimumContractAmt(defaults)
-        }], defaults.partyA_addr, defaults.primaryBrehon_addr))
-      .catch((err) => {
-        assert.isNotNull(err, "Exception was not thrown when primaryBrehon tried to raise a dispute");
-      });
-  });
+  it('by preventing primaryBrehon from raising a dispute', () =>
+    BrehonContract.deployed()
+    .then(startContractAndRaiseDispute(
+      [{
+        addr: defaults.partyA_addr,
+        value: getMinimumContractAmt(defaults),
+      }], defaults.partyA_addr, defaults.primaryBrehon_addr))
+    .catch((err) => {
+      assert.isNotNull(err, 'Exception was not thrown when primaryBrehon tried to raise a dispute');
+    }));
 
-  it('by preventing secondaryBrehon from raising a dispute', () => {
-    var brehonContract;
-    return BrehonContract.deployed()
-      .then(function captureReference(instance) {
-        brehonContract = instance;
-        return instance;
-      })
-      .then(startContractAndRaiseDispute(
-        [{
-          addr: defaults.partyA_addr,
-          value: getMinimumContractAmt(defaults)
-        }], defaults.partyA_addr, defaults.secondaryBrehon_addr))
-      .catch((err) => {
-        assert.isNotNull(err, "Exception was not thrown when secondaryBrehon tried to raise a dispute");
-      });
-  });
+  it('by preventing secondaryBrehon from raising a dispute', () =>
+    BrehonContract.deployed()
+    .then(startContractAndRaiseDispute(
+      [{
+        addr: defaults.partyA_addr,
+        value: getMinimumContractAmt(defaults),
+      }], defaults.partyA_addr, defaults.secondaryBrehon_addr))
+    .catch((err) => {
+      assert.isNotNull(err, 'Exception was not thrown when secondaryBrehon tried to raise a dispute');
+    }));
 
-  it('by preventing tertiaryBrehon from raising a dispute', () => {
-    var brehonContract;
-    return BrehonContract.deployed()
-      .then(function captureReference(instance) {
-        brehonContract = instance;
-        return instance;
-      })
-      .then(startContractAndRaiseDispute(
-        [{
-          addr: defaults.partyA_addr,
-          value: getMinimumContractAmt(defaults)
-        }], defaults.partyA_addr, defaults.tertiaryBrehon_addr))
-      .catch((err) => {
-        assert.isNotNull(err, "Exception was not thrown when tertiaryBrehon tried to raise a dispute");
-      });
-  });
+  it('by preventing tertiaryBrehon from raising a dispute', () =>
+    BrehonContract.deployed()
+    .then(startContractAndRaiseDispute(
+      [{
+        addr: defaults.partyA_addr,
+        value: getMinimumContractAmt(defaults),
+      }], defaults.partyA_addr, defaults.tertiaryBrehon_addr))
+    .catch((err) => {
+      assert.isNotNull(err, 'Exception was not thrown when tertiaryBrehon tried to raise a dispute');
+    }));
 
-  it('by preventing a rando from raising a dispute', () => {
-    var brehonContract;
-    return BrehonContract.deployed()
-      .then(function captureReference(instance) {
-        brehonContract = instance;
-        return instance;
-      })
-      .then(startContractAndRaiseDispute(
-        [{
-          addr: defaults.partyA_addr,
-          value: getMinimumContractAmt(defaults)
-        }], defaults.partyA_addr, accounts[6]))
-      .catch((err) => {
-        assert.isNotNull(err, "Exception was not thrown when a rando tried to raise a dispute");
-      });
-  });
+  it('by preventing a rando from raising a dispute', () =>
+    BrehonContract.deployed()
+    .then(startContractAndRaiseDispute(
+      [{
+        addr: defaults.partyA_addr,
+        value: getMinimumContractAmt(defaults),
+      }], defaults.partyA_addr, accounts[6]))
+    .catch((err) => {
+      assert.isNotNull(err, 'Exception was not thrown when a rando tried to raise a dispute');
+    }));
 });
 
-contract('BrehonContract should trigger ContractDisputed event', (accounts) => {
-  it('when dispute is first raised', () => {
-    var brehonContract;
-    return BrehonContract.deployed()
-      .then(function captureReference(instance) {
-        brehonContract = instance;
-        return instance;
-      })
-      .then(startContractAndRaiseDispute(
-        [{
-          addr: defaults.partyA_addr,
-          value: getMinimumContractAmt(defaults)
-        }], defaults.partyA_addr, defaults.partyA_addr))
-      .then((result) => {
-        var contractDisputedEvent = R.find(R.propEq('event', 'ContractDisputed'), result.logs);
-        assert.equal(contractDisputedEvent.args._disputingParty, defaults.partyA_addr,
-          "ContractDisputed event did not correctly provide the party disputed the contract");
-        assert.equal(contractDisputedEvent.args._activeBrehon, defaults.primaryBrehon_addr,
-          "ContractDisputed event did not correctly provide the deposits at the time of contract start");
-        assert.isDefined(contractDisputedEvent, "ContractStarted event was not emitted");
-      });
-  });
+contract('BrehonContract should trigger ContractDisputed event', () => {
+  it('when dispute is first raised', () =>
+    BrehonContract.deployed()
+    .then(startContractAndRaiseDispute(
+      [{
+        addr: defaults.partyA_addr,
+        value: getMinimumContractAmt(defaults),
+      }], defaults.partyA_addr, defaults.partyA_addr))
+    .then(verifyEvent('ContractDisputed', {
+      _disputingParty: defaults.partyA_addr,
+      _activeBrehon: defaults.primaryBrehon_addr,
+    })));
 });
 
-contract('BrehonContract should set primaryBrehon as the activeBrehon', (accounts) => {
+contract('BrehonContract should set primaryBrehon as the activeBrehon', () => {
   it('when dispute is first raised', () => {
-    return BrehonContract.deployed()
-      .then(function captureReference(instance) {
-        brehonContract = instance;
-        return instance;
-      })
-      .then(startContractAndRaiseDispute(
-        [{
-          addr: defaults.partyA_addr,
-          value: getMinimumContractAmt(defaults)
-        }], defaults.partyA_addr, defaults.partyA_addr))
-      .then(function verifyActiveBrehon() {
-        return brehonContract.activeBrehon.call().then((activeBrehon) => {
-            assert.equal(activeBrehon[BrehonStruct.contractAccepted], true, 'activeBrehon\'s contractAccepted should be set to true');
-            assert.equal(activeBrehon[BrehonStruct.addr], defaults.primaryBrehon_addr, 'activeBrehon\'s address not set to primaryBrehon correctly');
-            assert.equal(activeBrehon[BrehonStruct.fixedFee], defaults.primaryBrehon_fixedFee, 'activeBrehon\'s fixedFee not set to primaryBrehon\'s fixedFee');
-            assert.equal(activeBrehon[BrehonStruct.disputeFee], defaults.primaryBrehon_disputeFee, 'activeBrehon\'s disputeFee not set to primaryBrehon\'s disputeFee');
-        });
+    let brehonContract;
+    BrehonContract.deployed()
+    .then(function captureReference(instance) {
+      brehonContract = instance;
+      return instance;
+    })
+    .then(startContractAndRaiseDispute(
+      [{
+        addr: defaults.partyA_addr,
+        value: getMinimumContractAmt(defaults),
+      }], defaults.partyA_addr, defaults.partyA_addr))
+    .then(function verifyActiveBrehon() {
+      return brehonContract.activeBrehon.call().then((activeBrehon) => {
+        assert.equal(activeBrehon[BrehonStruct.contractAccepted], true, 'activeBrehon\'s contractAccepted should be set to true');
+        assert.equal(activeBrehon[BrehonStruct.addr], defaults.primaryBrehon_addr, 'activeBrehon\'s address not set to primaryBrehon correctly');
+        assert.equal(activeBrehon[BrehonStruct.fixedFee], defaults.primaryBrehon_fixedFee, 'activeBrehon\'s fixedFee not set to primaryBrehon\'s fixedFee');
+        assert.equal(activeBrehon[BrehonStruct.disputeFee], defaults.primaryBrehon_disputeFee, 'activeBrehon\'s disputeFee not set to primaryBrehon\'s disputeFee');
       });
+    });
   });
 });
