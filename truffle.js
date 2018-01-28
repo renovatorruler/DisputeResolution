339aa246b4f46d35e24bfb98c02198bddf118dec
Prashant Singh Pawar <prashantpawar@gmail.com>
2018-01-28 18:32:13 -0500

    - Working ropsten deployment

--- truffle.js
+++ truffle.js
@@ -1,23 +1,6 @@
-const bip39 = require('bip39');
-const hdkey = require('ethereumjs-wallet/hdkey');
-const ProviderEngine = require('web3-provider-engine');
-const WalletSubprovider = require('web3-provider-engine/subproviders/wallet.js');
-const Web3Subprovider = require('web3-provider-engine/subproviders/web3.js');
-const Web3 = require('web3');
+var HDWalletProvider = require("truffle-hdwallet-provider");
 
-const mnemonic = 'myth like bonus scare over problem client lizard pioneer submit female collect';
-const hdwallet = hdkey.fromMasterSeed(bip39.mnemonicToSeed(mnemonic));
-
-// Get the first account using the standard hd path.
-const walletHDpath = "m/44'/60'/0'/0/";
-const wallet = hdwallet.derivePath(walletHDpath + '0').getWallet();
-const address = '0x' + wallet.getAddress().toString('hex');
-
-const providerUrl = 'https://testnet.infura.io';
-const engine = new ProviderEngine();
-engine.addProvider(new WalletSubprovider(wallet, {}));
-engine.addProvider(new Web3Subprovider(new Web3.providers.HttpProvider(providerUrl)));
-engine.start(); // Required by the provider engine.
+let mnemonic = 'myth like bonus scare over problem client lizard pioneer submit female collect';
 
 module.exports = {
   build: 'webpack',
@@ -28,9 +11,9 @@ module.exports = {
       network_id: '*', // Match any network id
     },
     ropsten: {
-      network_id: 3,    // Official ropsten network id
-      provider: engine, // Use our custom provider
-      from: address,    // Use the address we derived
+      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"),
+      network_id: 3,
+      gas: 4700036,
     },
   },
 };
